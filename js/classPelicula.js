b95a4d3cfb46d359fe549215292b0e04a786432c
matias <pagosptcmatias@gmail.com>
2022-08-23 16:59:22 -0300

    agregamos la clase pelicula

---
+++ js/classPelicula.js
@@ -0,0 +1,10 @@
+export default class Pelicula {
+  constructor(codigo, titulo, descripcion, imagen, genero) {
+    this.codigo = codigo;
+    this.titulo = titulo;
+    this.descripcion = descripcion;
+    this.imagen = imagen;
+    this.genero = genero;
+  }
+//   agregar todos los metodos necesarios
+}
