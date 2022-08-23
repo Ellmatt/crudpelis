91834f142bf008fdc61ed1d2adf31b683676bad6
matias <pagosptcmatias@gmail.com>
2022-08-23 18:39:48 -0300

    agregamos localstorage

---
+++ js/app.js
@@ -0,0 +1,57 @@
+import Pelicula from "./classPelicula.js";
+
+// declarar  variables
+let listaPeliculas =
+  JSON.parse(localStorage.getItem("listaPeliculasKey")) || [];
+
+const modalFormPelicula = new bootstrap.Modal(
+  document.querySelector("#modalPelicula")
+);
+const btnCrearPelicula = document.querySelector("#btnCrearPelicula");
+let codigo = document.querySelector("#codigo");
+let descripcion = document.querySelector("#descripcion");
+let imagen = document.querySelector("#imagen");
+let genero = document.querySelector("#genero");
+let formulario = document.querySelector("#formPelicula");
+
+// agregar los eventis
+btnCrearPelicula.addEventListener("click", mostrarFormulario);
+formulario.addEventListener("submit", crearPelicula);
+
+function mostrarFormulario() {
+  modalFormPelicula.show();
+  codigo.value = uuidv4();
+  console.log(uuidv4());
+}
+
+function crearPelicula(e) {
+  e.preventDefault();
+  // agregar las validaciones
+  //  crear pelicula
+  const nuevaPelicula = new Pelicula(
+    codigo.value,
+    descripcion.value,
+    imagen.value,
+    genero.value
+  );
+  console.log(nuevaPelicula);
+  // guardar la pelicula en el arreglo
+  listaPeliculas.push(nuevaPelicula);
+  console.log(listaPeliculas);
+  // guqardar los datos en el localstorage
+  guardarDatosEnLS();
+  // limpiar el formulario
+  limpiarFormulario();
+  // cerrar la ventana modal
+  modalFormPelicula.hide();
+}
+
+function limpiarFormulario() {
+  formulario.reset();
+
+  // resetear la clase de bootstrap form-control
+}
+
+function guardarDatosEnLS() {
+  localStorage.setItem("listaPeliculasKey", JSON.stringify(listaPeliculas));
+}
